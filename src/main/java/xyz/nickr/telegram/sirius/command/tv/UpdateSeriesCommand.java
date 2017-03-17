ad90f09790e4a87c5a017acd746b31dd9ccfc4d1
Nick Robson <robson.nicholas1@gmail.com>
2017-03-17 14:08:34 +1100

    Don't get rate limited

--- src/main/java/xyz/nickr/telegram/sirius/command/tv/UpdateSeriesCommand.java
+++ src/main/java/xyz/nickr/telegram/sirius/command/tv/UpdateSeriesCommand.java
@@ -37,7 +37,9 @@ public class UpdateSeriesCommand extends Command {
                 edit(m, "_Updating..._ " + escape("0/" + size), ParseMode.MARKDOWN);
                 for (Series series : seriesSet) {
                     series.update();
-                    edit(m, "_Updating..._ " + escape(++n + "/" + size), ParseMode.MARKDOWN);
+                    if ((n % 5) == 0) {
+                        edit(m, "_Updating..._ " + escape(++n + "/" + size), ParseMode.MARKDOWN);
+                    }
                 }
                 reply(message, "Successfully updated all tracked shows!", ParseMode.NONE);
             } catch (Exception ex) {
